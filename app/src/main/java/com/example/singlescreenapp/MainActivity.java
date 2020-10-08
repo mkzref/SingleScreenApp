7452c48509c21f8d29439ce845372977f31a5d01
mkzref <marwahatamleh2@gmail.com>
2020-10-07 17:04:49 -0700

    projectchanged

---
+++ app/src/main/java/com/example/singlescreenapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.singlescreenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+
+Autor @ Marwa Hatamleh
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
